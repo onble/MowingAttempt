158956c66e9b2c65764ce34f519fe216d5d497e0
onble <62949816+onble@users.noreply.github.com>
2025-11-15 18:44:01 +0800

    10 增加tiled地图

--- assets/script/Battle.ts
+++ assets/script/Battle.ts
@@ -8,12 +8,12 @@ export class Battle extends Component {
     @property(Node) ndPlayer: Node;
     @property(Node) ndJoystick: Node;
 
+    @property(Node) ndCamera: Node;
+
     protected onEnable(): void {
-        this.ndJoystick
-            .getComponent(Joystick)
-            .onTouchEvent((radian: number) => {
-                this.ndPlayer.getComponent(Player).moveDirection = radian;
-            });
+        this.ndJoystick.getComponent(Joystick).onTouchEvent((radian: number) => {
+            this.ndPlayer.getComponent(Player).moveDirection = radian;
+        });
 
         this.ndPlayer.getComponent(Player).isMoving = true;
     }
@@ -22,5 +22,8 @@ export class Battle extends Component {
 
     start() {}
 
-    update(deltaTime: number) {}
+    update(deltaTime: number) {
+        // 让摄像头的位置一直跟着玩家
+        this.ndCamera.worldPosition = this.ndPlayer.worldPosition;
+    }
 }
--- assets/script/Player.ts
+++ assets/script/Player.ts
@@ -3,19 +3,24 @@ const { ccclass, property } = _decorator;
 
 @ccclass("Player")
 export class Player extends Component {
+    /**
+     * 速度
+     */
     speed: number = 1;
+    /**
+     * 方向
+     */
     moveDirection: number = 0;
+    /**
+     * 运动状态
+     */
     isMoving: boolean = false;
     start() {}
 
     update(deltaTime: number) {
         if (this.isMoving) {
-            const x =
-                this.node.position.x +
-                Math.cos(this.moveDirection) * this.speed;
-            const y =
-                this.node.position.y +
-                Math.sin(this.moveDirection) * this.speed;
+            const x = this.node.position.x + Math.cos(this.moveDirection) * this.speed;
+            const y = this.node.position.y + Math.sin(this.moveDirection) * this.speed;
             this.node.setPosition(x, y);
         }
     }
